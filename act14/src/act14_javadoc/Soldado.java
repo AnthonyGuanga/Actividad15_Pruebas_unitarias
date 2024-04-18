bb37381bfe6b3a53be0c8a7c96aee7fa1e13a1bb
FelipeGarcia <felipe.garcia.costa@bootcamp-upgrade.com>
2024-04-18 13:10:06 +0200

    agregué la documentación de Soldado

--- act14/src/act14_javadoc/Soldado.java
+++ act14/src/act14_javadoc/Soldado.java
@@ -8,6 +8,12 @@ public class Soldado {
      //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y
      //la documentación. Aquí no se han creado porque no aportan nada
      
+     
+     /**
+      * Método que sirve para saber si el soldado puede disparar, en el mismo se evalúa el atributo numeroBalas  y en caso de que 
+      * sea positivo devolverá true, caso contrario devolverá false.
+      * @return devuelve true si numeroBalas es positivo, sino false.
+      */
      public boolean puedeDisparar() {
     	 if(this.numeroBalas > 0) {
     		 
@@ -18,10 +24,37 @@ public class Soldado {
      
      }
      
+     /**
+      * Metodó que sirve para que un Soldado dispare a otro Soldado pasado por parámetro.
+      * Decrementa en 1 el número de balas del propio Soldado y valoraliza el valor del atributo estaMuerto 
+      * del Soldado pasado por parámtero en TRUE.
+      * @param sol
+      */
+     
      public void disparar(Soldado sol) {
     	 this.numeroBalas--;
     	 
     	 sol.estaMuerto = true;
      }
+
+     
+    //}--------------GETTERS AND SETTERS---------------{
+	public boolean isEstaMuerto() {
+		return estaMuerto;
+	}
+
+	public void setEstaMuerto(boolean estaMuerto) {
+		this.estaMuerto = estaMuerto;
+	}
+
+	public int getNumeroBalas() {
+		return numeroBalas;
+	}
+
+	public void setNumeroBalas(int numeroBalas) {
+		this.numeroBalas = numeroBalas;
+	}
+    //}--------------GETTERS AND SETTERS---------------{
+     
 
 }
